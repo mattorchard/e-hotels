f4322769df04a75e78152ff5680a4178007e04b8
Matthew <morch028@uottawa.ca>
2019-02-17 18:46:38 -0500

    Adds basic components for navigating admin page

--- front-end/src/pages/AdminPage.js
+++ front-end/src/pages/AdminPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import HotelChainManager from "../components/AdminComponents/HotelChainManager";
+import {simulateDelay} from "../services/simulator-service";
+import {AsyncList} from "../components/AsyncList";
+export default class AdminPage extends React.Component {
 
-export default class CustomerPage extends React.Component {
+  state = {
+    loadingHotelChains: true,
+    hotelChains: []
+  };
+
+  async componentDidMount() {
+    await simulateDelay(500);
+    const hotelChains = [{id: 1}];
+    this.setState({hotelChains, loadingHotelChains: false});
+  }
+
   render() {
-    return "AP";
+    return <main>
+      <h2>Hotel Chains</h2>
+      <button type="button">
+        + Add Hotel Chain
+      </button>
+      <AsyncList loading={this.state.loadingHotelChains}
+                 placeholderMessage="No hotel chains."
+                 loadingMessage="Loading hotel chains...">
+        {this.state.hotelChains.map(chain =>
+          <li key={chain.id}><HotelChainManager {...chain}/></li>)}
+      </AsyncList>
+
+    </main>;
   }
 }
