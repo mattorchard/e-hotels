6ac66ff5fe3b8434acde4ada239d68efdfb755f7
Matthew <morch028@uottawa.ca>
2019-02-17 22:30:34 -0500

    Adds basic customer page

--- front-end/src/pages/CustomerPage.js
+++ front-end/src/pages/CustomerPage.js
@@ -1,7 +1,34 @@
 import React from "react";
+import {AsyncList} from "../components/AsyncList";
+import {simulateDelay} from "../services/simulator-service";
+import CustomerManager from "../components/CustomerComponents/CustomerManager";
 
 export default class CustomerPage extends React.Component {
+  state = {
+    loadingCustomers: true,
+    customers: []
+  };
+
+  async componentDidMount() {
+    await simulateDelay(1000);
+    const customers = [{sinOrSsn: 808, givenName: "Harvey", familyName: "Treacher"}];
+    this.setState({customers, loadingCustomers: false})
+  }
+
   render() {
-    return "CP";
+    return <main className="main-content">
+      <h2>Customer Operations</h2>
+      <h3>Search Availability</h3>
+      <h3>Rooms by Location</h3>
+      <h3>Hotel Capacity</h3>
+      <h2>Customers</h2>
+      <AsyncList loading={this.state.loadingCustomers}
+                 placeholderMessage="No customers"
+                 loadingMessage="Loading customers...">
+        {this.state.customers.map(customer =>
+          <li key={customer.sinOrSsn}><CustomerManager {...customer}/></li>)}
+      </AsyncList>
+
+    </main>;
   }
 }
