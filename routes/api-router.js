6df06d493f6fe042ac33a2bc5362476ff07e173a
morch028 <morch028@uottawa.ca>
2019-02-21 23:12:42 -0500

    Adds backend function for fetching hotel chains

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
+const {getHotelChains} = require('./hotel-chain-router');
+
 
 router.get('/test', (req, res) =>
   res.send({"Howdy": "World"})
 );
 
+router.get("/hotel-chains", getHotelChains);
+
 router.use((req, res, next) =>
-  next(new createError.NotFound("API Endpoint not found"))
+  next(new createError.NotFound(`API Endpoint not found [${req.url}]`))
 );
 
 module.exports = router;
