90952cb9beb2334255501712d0e8b15c161fdd92
morch028 <morch028@uottawa.ca>
2019-03-02 11:18:58 -0500

    Adds endpoint to fetch employees

--- routes/api-router.js
+++ routes/api-router.js
@@ -4,6 +4,7 @@ const createError = require('http-errors');
 const {getHotelChains} = require('./hotel-chain-router');
 const {getHotels} = require('./hotel-router');
 const {getRooms} = require('./room-router');
+const {getEmployees} = require('./employee-router');
 
 
 router.get('/test', (req, res) =>
@@ -15,6 +16,8 @@ router.get("/hotel-chains", getHotelChains);
 router.get("/hotel-chains/:hotelChainName/hotels", getHotels);
 
 router.get("/hotel-chains/:hotelChainName/:hotelId/rooms", getRooms);
+
+router.get("/employees", getEmployees);
 
 router.use((req, res, next) =>
   next(new createError.NotFound(`API Endpoint not found [${req.url}]`))
--- routes/employee-router.js
+++ routes/employee-router.js
@@ -1,3 +1,33 @@
+const {responseToRows, nestAddress} = require('../services/postgres-service');
+const {Pool} = require('pg');
+const pool = new Pool();
+const lodash = require("lodash");
+
+const getEmployees = async (req, res, next) => {
+  try {
+    const employeesPromise = pool.query(`SELECT * FROM employee, address WHERE employee.address_id = address.id`);
+    const rolesPromise = pool.query(`SELECT * FROM employee_role`);
+    // Await the query results and convert their casing
+    const [employeeRows, roleRows] = responseToRows(await Promise.all([employeesPromise, rolesPromise]));
+
+    // Create an address field under each employee
+    const employees = employeeRows.map(nestAddress);
+
+    // Create a list of roles under each employee
+    const rolesByEmployee = lodash.groupBy(roleRows, "employeeId");
+    employees.forEach(employee =>
+      employee.roles = (rolesByEmployee[employee.id] || []).map(r => r.role));
+
+    // Group employees by their hotel chain
+    const employeesByHotelChainName = lodash.groupBy(employees, "hotelChainName");
+    res.send(employeesByHotelChainName);
+  } catch (error) {
+    console.error("Unable to fetch employees", error);
+    next(error);
+  }
+};
 // Add an employee
 // Edit an employee
 // Delete an employee
+
+module.exports = {getEmployees};
