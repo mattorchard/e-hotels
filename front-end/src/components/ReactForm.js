8680a214e09b03f4b1a55c036b0f943791f057e1
morch028 <morch028@uottawa.ca>
2019-03-04 11:03:06 -0500

    Adds primitive for form manipulation

---
+++ front-end/src/components/ReactForm.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export default class ReactForm extends Component {
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+}
