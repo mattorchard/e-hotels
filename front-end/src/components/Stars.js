f1e7ee636aa2fed9fa27e1018c9ccfa582c160f0
morch028 <morch028@uottawa.ca>
2019-03-05 17:05:16 -0500

    Changes radio stars to use loop instead of duplicated markup

--- front-end/src/components/Stars.js
+++ front-end/src/components/Stars.js
@@ -3,39 +3,20 @@ import React from "react";
 export default class Stars extends React.Component {
 
   render() {
+    const {number, children, disabled, name} = this.props;
     return <div className="radio-star-group">
-      {this.props.children}
+      {children}
+      {[1, 2, 3, 4, 5].map(category =>
       <input type="radio"
              className="radio-star"
-             value="1"
-             aria-label="1 Star"
-             title="1 Star"
-             name={this.props.name}/>
-      <input type="radio"
-             className="radio-star"
-             value="2"
-             aria-label="2 Star"
-             title="2 Star"
-             name={this.props.name}/>
-      <input type="radio"
-             defaultChecked
-             className="radio-star"
-             value="3"
-             aria-label="3 Star"
-             title="3 Star"
-             name={this.props.name}/>
-      <input type="radio"
-             className="radio-star"
-             value="4"
-             aria-label="4 Star"
-             title="4 Star"
-             name={this.props.name}/>
-      <input type="radio"
-             className="radio-star"
-             value="5"
-             aria-label="5 Star"
-             title="5 Star"
-             name={this.props.name}/>
+             key={category}
+             value={category}
+             aria-label={category + " Star"}
+             title={category + " Star"}
+             defaultChecked={number === category}
+             disabled={disabled}
+             name={name}/>
+      )}
     </div>
   }
 }
