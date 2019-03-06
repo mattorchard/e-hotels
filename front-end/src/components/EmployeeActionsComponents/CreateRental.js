96db9307877e65942f09b42bf0127f3588d48722
morch028 <morch028@uottawa.ca>
2019-03-06 16:58:21 -0500

    Adds call to save rentals

--- front-end/src/components/EmployeeActionsComponents/CreateRental.js
+++ front-end/src/components/EmployeeActionsComponents/CreateRental.js
@@ -6,11 +6,23 @@ import {toast} from "react-toastify";
 export default class CreateRental extends React.Component {
 
   saveRental = async rental => {
-    console.log(rental);
     try {
-      throw new Error();
+      const {hotelChainName, hotelId, roomNumber, ...body} = rental;
+      const response = await fetch(`/api/hotel-chains/${hotelChainName}/${hotelId}/${roomNumber}/rent`, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(body)
+      });
+      if (!response.ok) {
+        if (response.status === 409) {
+          toast.error("That room is unavailable at that time");
+        }
+        throw new Error("Unable to create rental");
+      }
+      toast.success("Rental created");
+      this.props.onRequestClose();
     } catch (error) {
-      toast.error(JSON.stringify(rental))
+      toast.error("Unable to create rental");
     }
   };
 
