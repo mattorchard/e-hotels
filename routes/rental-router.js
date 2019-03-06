951c21e3a5542148e92b2817045ea8f437d4ef2b
morch028 <morch028@uottawa.ca>
2019-03-06 17:00:38 -0500

    Moves room in use helper to common booking service

--- routes/rental-router.js
+++ routes/rental-router.js
@@ -1,31 +1,11 @@
 const {responseToRows} = require('../services/postgres-service');
+const {roomInUse} = require('../services/booking-service');
 const {Pool} = require('pg');
 const pool = new Pool();
 const createError = require('http-errors');
 
 // Check in (booking to rental)
 // Add rental
-
-const roomInUse = async(pool, hotelChainName, hotelId, roomNumber, startDate, endDate) => {
-  const response = await pool.query(
-    `(SELECT room_number FROM rental
-    WHERE hotel_chain_name = $1 AND hotel_id = $2 AND room_number = $3
-    AND (
-      $4 BETWEEN start_date AND end_date
-      OR $5 BETWEEN start_date AND end_date
-      OR start_date BETWEEN $4 AND $5
-      OR end_date BETWEEN $4 AND $5
-    )) UNION
-    (SELECT room_number FROM booking
-    WHERE hotel_chain_name = $1 AND hotel_id = $2 AND room_number = $3
-    AND (
-      $4 BETWEEN start_date AND end_date
-      OR $5 BETWEEN start_date AND end_date
-      OR start_date BETWEEN $4 AND $5
-      OR end_date BETWEEN $4 AND $5
-    ))`, [hotelChainName, hotelId, roomNumber, startDate, endDate]);
-  return response.rows.length > 0;
-};
 
 const createRental = async(req, res, next) => {
   const {hotelChainName, hotelId, roomNumber} = req.params;
