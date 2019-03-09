90e263c67b1d7a357f59c6f29c5bdd21ed7a72ae
morch028 <morch028@uottawa.ca>
2019-03-09 11:55:34 -0500

    Adds capacity to hotel fetching service

--- services/hotel-service.js
+++ services/hotel-service.js
@@ -3,10 +3,12 @@ const {responseToRows, nestAddress, nestManager} = require('../services/postgres
 const getHotels = async client => {
   const response = await client.query(
     `SELECT *, (
-      SELECT COUNT(room_number) AS num_rooms FROM room WHERE hotel_id = hotel.id
-    ) FROM address, employee, hotel
+      SELECT COUNT(room_number) AS num_rooms FROM room WHERE room.hotel_id = hotel.id
+    ) FROM address, employee, hotel, capacity_by_hotel
     WHERE address.id = hotel.address_id
-    AND employee.id = hotel.manager_id`);
+    AND employee.id = hotel.manager_id
+    AND capacity_by_hotel.hotel_chain_name = hotel.hotel_chain_name
+    AND capacity_by_hotel.hotel_id = hotel.id`);
   return responseToRows(response)
     .map(nestAddress)
     .map(nestManager);
