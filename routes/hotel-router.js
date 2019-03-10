9637b2a28371e04028044e8365f5a76c6b20aeab
morch028 <morch028@uottawa.ca>
2019-03-09 23:53:20 -0500

    Adds queries for email addresses and phone numbers of hotels

--- routes/hotel-router.js
+++ routes/hotel-router.js
@@ -2,6 +2,7 @@ const {responseToRows, nestAddress, nestManager} = require('../services/postgres
 const {Pool} = require('pg');
 const pool = new Pool();
 const createError = require('http-errors');
+const lodash = require("lodash");
 
 
 const getHotels = async (req, res, next) => {
@@ -10,7 +11,7 @@ const getHotels = async (req, res, next) => {
     return next(new createError.UnprocessableEntity("Must supply hotel chain name"));
   }
   try {
-    const response = await pool.query(
+    const hotelPromise = pool.query(
       `SELECT * FROM address, employee, capacity_by_hotel, hotel
       WHERE hotel.hotel_chain_name = $1
       AND hotel.address_id = address.id
@@ -18,8 +19,28 @@ const getHotels = async (req, res, next) => {
       AND hotel.id = capacity_by_hotel.hotel_id
       AND hotel.hotel_chain_name = capacity_by_hotel.hotel_chain_name`,
       [hotelChainName]);
-    const rows = responseToRows(response);
-    const hotels = rows.map(nestAddress).map(nestManager);
+    const phonePromise = pool.query(
+      `SELECT * FROM hotel_phone_number
+      WHERE hotel_chain_name = $1`,
+      [hotelChainName]);
+    const emailPromise = pool.query(
+      `SELECT * FROM hotel_email_address
+      WHERE hotel_chain_name = $1`,
+      [hotelChainName]);
+
+    const responses = await Promise.all([hotelPromise, phonePromise, emailPromise]);
+    const [hotelRows, phoneRows, emailRows] = responseToRows(responses);
+
+    const hotels = hotelRows.map(nestAddress).map(nestManager);
+
+    const groupedPhones = lodash.groupBy(phoneRows, "hotelId");
+    const groupedEmails = lodash.groupBy(emailRows, "hotelId");
+
+    hotels.forEach(hotel => {
+      hotel.phoneNumbers = (groupedPhones[hotel.id] || []).map(p => p.phoneNumber);
+      hotel.emailAddresses = (groupedEmails[hotel.id] || []).map(e => e.emailAddress);
+    });
+
     res.send(hotels);
   } catch (error) {
     console.error("Unable to fetch hotels", error);
