33a979389f784af05e4c19133d648f1def821174
morch028 <morch028@uottawa.ca>
2019-03-17 23:29:19 -0400

    Fixed address update silently failing when not passed ID

--- services/address-service.js
+++ services/address-service.js
@@ -4,12 +4,15 @@ const insertAddress = async (client, {streetNumber, streetName, city, country}) 
   return response.rows[0].id
 };
 
-const updateAddress = async (client, {id, streetNumber, streetName, city, country}) =>
+const updateAddress = async (client, {id, streetNumber, streetName, city, country}) => {
+  if (!id) {
+    throw new Error("Cannot update address without ID");
+  }
   await client.query(
     `UPDATE address
      SET street_number = $1, street_name = $2, city = $3, country = $4
      WHERE id = $5`,
     [streetNumber, streetName, city, country, id]);
-
+};
 
 module.exports = {insertAddress, updateAddress};
